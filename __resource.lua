9863d508e8b06096aedacf7e02dbbe124508cc9d
Andmarski <59533553+Anmaal@users.noreply.github.com>
2022-01-03 17:16:16 +0100

    Initial commit

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+files {
+    'index.html',
+    'css/style.css',
+    'png.png',
+    'img/background-1.png',
+    'img/background-2.jpg',
+    'img/background-3.jpg',
+    'img/background-4.jpg',
+    'Discord.png',
+    'audio.mp3'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
